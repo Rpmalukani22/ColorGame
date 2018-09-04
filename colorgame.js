f7cf14f7a79b13e12be387f5112d1b03c8653b56
Ruchitesh <Ruchiteshmalukani@gmail.com>
2018-09-04 22:56:47 +0530

    Result Feature added.

--- colorgame.js
+++ colorgame.js
@@ -5,6 +5,7 @@ for(var i=0;i<6;i++){
   temp="rgb("+Math.floor(Math.random()*255)+", "+Math.floor(Math.random()*255)+", "+Math.floor(Math.random()*255)+")";
   x.push(temp);
 }
+var message=document.getElementById("result");
 y=document.querySelectorAll(".rect");
 pickedcolor=x[Math.floor(Math.random()*6)];
 putQuestion=document.getElementById("question");
@@ -14,7 +15,18 @@ i.addEventListener('click',()=>{
   console.log(pickedcolor.toUpperCase());
   console.log(i.style.backgroundColor);
   if(pickedcolor.toUpperCase()===i.style.backgroundColor.toUpperCase())
-  {i.style.visibility='hidden';
+  {
+  //message success
+  message.textContent="Correct!!"
+  for(let k=0;k<y.length;k++){
+    y[k].style.visibility="visible";
+    y[k].style.backgroundColor=pickedcolor;
+  }
+}
+else{
+  message.textContent="Uh oh wrong!Try again.";
+  i.style.visibility='hidden';
+  //message=try again
 }
 });
 });
