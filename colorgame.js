cb4e7c9f9a101a901b729cfa8d445d0a0a4483ce
Ruchitesh <Ruchiteshmalukani@gmail.com>
2018-09-07 19:55:28 +0530

    added Easy and Hard levels

--- colorgame.js
+++ colorgame.js
@@ -1,26 +1,43 @@
-x=[]
-for(var i=0;i<6;i++){
+var x=[]
+var y=document.querySelectorAll(".rect");
+var pickedcolor="";
+var message=document.getElementById("result");
+var putQuestion=document.getElementById("question");
+generate=(x,y,n)=>{
+  window.pickedcolor="";
+  window.message.textContent="";
+  x=[]
+for(var i=0;i<n;i++){
+  y[i].style.visibility="visible";
   temp="rgb("+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+")";
   while(x.indexOf(temp)!=-1)
   temp="rgb("+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+")";
   x.push(temp);
 }
-var message=document.getElementById("result");
-y=document.querySelectorAll(".rect");
-pickedcolor=x[Math.floor(Math.random()*6)];
-putQuestion=document.getElementById("question");
-putQuestion.textContent=pickedcolor.toUpperCase();
-y.forEach((i,ind)=>{i.style.backgroundColor=x[ind];
+if(n===3)
+{for(var i=3;i<6;i++)
+y[i].style.visibility="hidden";
+}
+window.pickedcolor=x[Math.floor(Math.random()*n)];
+window.putQuestion.textContent=pickedcolor.toUpperCase();
+window.y.forEach((i,ind)=>{i.style.backgroundColor=x[ind];
 i.addEventListener('click',()=>{
   console.log(pickedcolor.toUpperCase());
   console.log(i.style.backgroundColor);
   if(pickedcolor.toUpperCase()===i.style.backgroundColor.toUpperCase())
   {
   //message success
+  console.log(n);
   message.textContent="Correct!!"
-  for(let k=0;k<y.length;k++){
-    y[k].style.visibility="visible";
+  for(let k=0;k<6;k++){
+    console.log("k :",k,"n :",n);
+    if(k<n)
+    {y[k].style.visibility="visible";
     y[k].style.backgroundColor=pickedcolor;
+  }
+  else{
+    y[k].style.visibility="hidden";
+  }
   }
 }
 else{
@@ -30,3 +47,11 @@ else{
 }
 });
 });
+return x;
+}
+x=generate(x,y,6);
+var lvl=document.querySelectorAll(".level");
+console.log(lvl);
+lvl[0].addEventListener('click',()=>{x=generate(x,y,3);console.log(x);});
+lvl[1].addEventListener('click',()=>{x=generate(x,y,6);console.log(x);});
+console.log(x);
