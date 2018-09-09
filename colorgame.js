d5176b543fbbbce6dba4b91edb90ec3ccff5b41a
Ruchitesh <Ruchiteshmalukani@gmail.com>
2018-09-09 20:04:11 +0530

    Bug fixed.Transition effects added.

--- colorgame.js
+++ colorgame.js
@@ -6,7 +6,10 @@ var message=document.getElementById("result");
 var putQuestion=document.getElementById("question");
 var lvl=document.querySelectorAll(".level");
 var h1=document.querySelector("h1");
-var lst1=[],lst2=[];
+var fillcolour=(x,n)=>{
+  for(var i=0;i<n;i++)
+  y[i].style.backgroundColor=x[i];
+}
 changeclr=(n)=>{
   if(n===3)
   {
@@ -44,40 +47,11 @@ for(var i=0;i<n;i++){
 }
 if(n===3)
 {for(var i=3;i<6;i++)
-y[i].style.visibility="hidden";
+y[i].style.backgroundColor="Black";
 }
 window.pickedcolor=x[Math.floor(Math.random()*n)];
 window.putQuestion.textContent=pickedcolor.toUpperCase();
-window.y.forEach((i,ind)=>{i.style.backgroundColor=x[ind];
-i.addEventListener('click',()=>{
-  console.log(pickedcolor.toUpperCase());
-  console.log(i.style.backgroundColor);
-  if(pickedcolor.toUpperCase()===i.style.backgroundColor.toUpperCase())
-  {
-  //message success
-  console.log(n);
-  message.textContent="Correct!!"
-  window.lvl[0].textContent="Play again?"
-  for(let k=0;k<6;k++){
-    console.log("k :",k,"n :",n);
-    if(k<n)
-    {y[k].style.visibility="visible";
-    y[k].style.backgroundColor=pickedcolor;
-    h1.style.backgroundColor=pickedcolor;
-    changeclr(n);
-  }
-  else{
-    y[k].style.visibility="hidden";
-  }
-  }
-}
-else{
-  message.textContent="Uh oh wrong!Try again.";
-  i.style.visibility='hidden';
-  //message=try again
-}
-});
-});
+fillcolour(x,n);
 return x;
 }
 x=generate(x,y,6);
@@ -86,3 +60,33 @@ lvl[1].addEventListener('click',()=>{window.size=3;x=generate(x,y,window.size);c
 lvl[2].addEventListener('click',()=>{window.size=6;x=generate(x,y,window.size);console.log(x);});
 lvl[0].addEventListener('click',()=>{x=generate(x,y,window.size);console.log(x);});
 console.log(x);
+window.y.forEach((i,ind)=>{
+i.addEventListener('click',()=>{
+  console.log(pickedcolor.toUpperCase());
+  console.log(i.style.backgroundColor);
+  if(pickedcolor.toUpperCase()===i.style.backgroundColor.toUpperCase())
+  {
+  //message success
+  console.log(size);
+  message.textContent="Correct!!"
+  window.lvl[0].textContent="Play again?"
+  for(let k=0;k<6;k++){
+    console.log("k :",k,"size :",size);
+    if(k<size)
+    {y[k].style.visibility="visible";
+    y[k].style.backgroundColor=pickedcolor;
+    h1.style.backgroundColor=pickedcolor;
+    changeclr(size);
+  }
+  else{
+    y[k].style.backgroundColor="Black";
+  }
+  }
+}
+else{
+  message.textContent="wrong!Try again.";
+  i.style.backgroundColor='Black';
+  //message=try again
+}
+});
+});
